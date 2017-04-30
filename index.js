3d652214e64363c6b43b6e149f93d4199aef4679
Kimmo Saari <kimmo.saari@revolt.fi>
2017-04-30 05:00:12 +0300

    Few fixes

--- index.js
+++ index.js
@@ -110,7 +110,7 @@ const createWindow = () => {
   win.focus();
 
   // Open the DevTools.
-  win.webContents.openDevTools()
+  // win.webContents.openDevTools();
 
   // Emitted when the window is closed.
   win.on('closed', () => {
